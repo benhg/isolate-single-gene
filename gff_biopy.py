2e4fd747bae27bc0a87da0fafa1859c6213688fa
Ben Glick <glick@glick.cloud>
2020-04-20 16:00:44 -0700

    debugging

--- gff_biopy.py
+++ gff_biopy.py
@@ -15,7 +15,7 @@ for rec in GFF.parse(in_handle, base_dict=seq_dict):
     for feature in rec.features:
         if feature.qualifiers["Name"][0] == "18S_rRNA":
             feature_loc = feature.location
-            print(feature_loc[0])
+            print(feature_loc.__dict__.keys())
             guess_number += 1
             if guess_number == 2:
                 guess_number = 0
