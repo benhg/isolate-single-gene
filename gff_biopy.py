f22ebe1d637089d29509dd7c593de1757d6699a9
Ben Glick <glick@glick.cloud>
2020-04-20 15:41:24 -0700

    change filename

--- gff_biopy.py
+++ gff_biopy.py
@@ -6,7 +6,7 @@ in_seq_handle = open(in_seq_file)
 seq_dict = SeqIO.to_dict(SeqIO.parse(in_seq_handle, "fasta"))
 in_seq_handle.close()
 
-in_file = "locations.gff3"
+in_file = "input_gff3.gff3"
 in_handle = open(in_file)
 #for rec in GFF.parse(in_handle, base_dict=seq_dict):
 #    sequence = rec.seq
