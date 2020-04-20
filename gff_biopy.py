8e456de2779da9f5cfc6ba7b12250871fcbecca9
Ben Glick <glick@glick.cloud>
2020-04-20 15:53:23 -0700

    debugging

--- gff_biopy.py
+++ gff_biopy.py
@@ -14,7 +14,7 @@ for rec in GFF.parse(in_handle, base_dict=seq_dict):
     # print(rec)
     print(rec.description)
     for feature in rec.features:
-    	print(feature.qualifiers.__dict__.keys())
+    	print(feature.qualifiers.keys())
 
 #in_gff = GFF.parse(in_handle, base_dict=seq_dict)
 
