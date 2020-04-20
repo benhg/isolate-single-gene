d8bb4da09fe33a970cb3ac92fcd96bcdca8fbf7f
Ben Glick <glick@glick.cloud>
2020-04-20 15:45:48 -0700

    debugging

--- gff_biopy.py
+++ gff_biopy.py
@@ -12,7 +12,7 @@ for rec in GFF.parse(in_handle, base_dict=seq_dict):
     sequence = rec.seq
     taxon = rec.id
     print(rec)
-    print(dict(rec))
+    print(rec.__dict__.keys())
 
 #in_gff = GFF.parse(in_handle, base_dict=seq_dict)
 
