a029d23853c7960712c07b91bfc22a54ffa3e8b8
Ben Glick <glick@glick.cloud>
2020-04-20 16:07:25 -0700

    file formatting

--- gff_biopy.py
+++ gff_biopy.py
@@ -6,22 +6,24 @@ in_seq_handle = open(in_seq_file)
 seq_dict = SeqIO.to_dict(SeqIO.parse(in_seq_handle, "fasta"))
 in_seq_handle.close()
 
-in_file = "raw_barrnap.gff3"
-in_handle = open(in_file)
-for rec in GFF.parse(in_handle, base_dict=seq_dict):
-    taxon = rec.description
-    sequence = str(rec.seq)
-    guess_number = 0
-    print(rec.description)
-    for feature in rec.features:
-        if feature.qualifiers["Name"][0] == "18S_rRNA":
-            feature_loc = feature.location
-            start = int(feature_loc._start)
-            end = int(feature_loc._end)
-            print(sequence[start:end])
-            guess_number += 1
-            if guess_number == 2:
-                guess_number = 0
+with open("IPedibility_18S_guesses.fasta", "w") as fh:
+    in_file = "raw_barrnap.gff3"
+    in_handle = open(in_file)
+    for rec in GFF.parse(in_handle, base_dict=seq_dict):
+        taxon = rec.description
+        sequence = str(rec.seq)
+        guess_number = 0
+        print(rec.description)
+        for feature in rec.features:
+            if feature.qualifiers["Name"][0] == "18S_rRNA":
+                feature_loc = feature.location
+                start = int(feature_loc._start)
+                end = int(feature_loc._end)
+                feature = sequence[start:end]
+                fh.write(f">{taxon}, guess {guess_number}\n{feature}\n")
+                guess_number += 1
+                if guess_number == 2:
+                    guess_number = 0
 
 #in_gff = GFF.parse(in_handle, base_dict=seq_dict)
 
