4874938f342d652ef97f75750527b1e0d7e46559
Ben Glick <glick@glick.cloud>
2020-04-21 10:45:57 -0700

    syntax errprs and flake8

--- gff_biopy.py
+++ gff_biopy.py
@@ -2,7 +2,6 @@ from BCBio import GFF
 from Bio import SeqIO
 
 
-
 def extract(locations, in_db, out_file, filter_name=None):
 
     in_seq_handle = open(in_db)
@@ -22,21 +21,38 @@ def extract(locations, in_db, out_file, filter_name=None):
                         start = int(feature_loc._start)
                         end = int(feature_loc._end)
                         feature = sequence[start:end]
-                        fh.write(f">{taxon}, guess {guess_number}\n{feature}\n")
+                        fh.write(
+                            f">{taxon}\n{feature}\n")
                 else:
                     feature_loc = feature.location
                     start = int(feature_loc._start)
                     end = int(feature_loc._end)
                     feature = sequence[start:end]
-                    fh.write(f">{taxon}, guess {guess_number}\n{feature}\n")
+                    fh.write(f">{taxon}\n{feature}\n")
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--locations", required=True, type=str, help="Path to locations file in GFF3 format")
-    parser.add_argument("--db", type=str, required=True, help="Path to database file in FASTA format")
-    parser.add_argument("--out", type=str, required=True, help="Path to output file in FASTA format (needs not exist)")
-    parser.add_argument("--filter", type=str, required=False, help="Name of type of feature to extract. Stored as a name in the qualifiers section. Example: '18S_rRNA'")
+    parser.add_argument(
+        "--locations",
+        required=True,
+        type=str,
+        help="Path to locations file in GFF3 format")
+    parser.add_argument(
+        "--db",
+        type=str,
+        required=True,
+        help="Path to database file in FASTA format")
+    parser.add_argument(
+        "--out",
+        type=str,
+        required=True,
+        help="Path to output file in FASTA format (needs not exist)")
+    parser.add_argument(
+        "--filter",
+        type=str,
+        required=False,
+        help="Name of type of feature to extract. Stored as a name in the qualifiers section. Example: '18S_rRNA'")
     args = parser.parse_args()
     extract(args.locations, args.db, args.out, args.filter)
