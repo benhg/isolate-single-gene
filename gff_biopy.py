7992e3a82b19156e3aaaaf065eecd900cdd1f92b
Ben Glick <glick@glick.cloud>
2020-04-21 10:51:08 -0700

    tag output with names

--- gff_biopy.py
+++ gff_biopy.py
@@ -16,19 +16,20 @@ def extract(locations, in_db, out_file, filter_name=None):
             for feature in rec.features:
                 # Filter by feature qualifiers name if provided
                 if filter_name:
+                    description = " ".join(feature.qualifiers["Name"])
                     if feature.qualifiers["Name"][0] == filter_name:
                         feature_loc = feature.location
                         start = int(feature_loc._start)
                         end = int(feature_loc._end)
                         feature = sequence[start:end]
                         fh.write(
-                            f">{taxon}\n{feature}\n")
+                            f">{taxon},{description}\n{feature}\n")
                 else:
                     feature_loc = feature.location
                     start = int(feature_loc._start)
                     end = int(feature_loc._end)
                     feature = sequence[start:end]
-                    fh.write(f">{taxon}\n{feature}\n")
+                    fh.write(f">{taxon},{description}\n{feature}\n")
 
 
 if __name__ == '__main__':
